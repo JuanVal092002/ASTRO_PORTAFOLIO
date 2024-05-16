d819f7488936184d8dc3062190efc410d6ab1cb8
JuanVal092002 <juancarlospastasvalencia@hotmail.com>
2024-05-16 02:02:56 -0500

    Mi primer portafolio

---
+++ astro-portfolio-main/src/lib/constants.ts
@@ -0,0 +1,78 @@
+import type { Site, Page } from './types'
+
+export const loaderAnimation = [
+  '.loader',
+  { opacity: [1, 0], pointerEvents: 'none' },
+  { easing: 'ease-out' },
+]
+
+export const LINKS = {
+  github: 'https://github.com/JuanVal092002',
+  linkedin: 'https://www.linkedin.com/in/juanval092002/',
+  mail: 'juancarlospastasvalencia@hotmail.com',
+}
+
+// Global
+export const SITE: Site = {
+  TITLE: 'Astro Sphere',
+  DESCRIPTION:
+    'Welcome to Astro Sphere, a portfolio and blog for designers and developers.',
+  AUTHOR: 'Mark Horn',
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: 'Work',
+  DESCRIPTION: 'Places I have worked.',
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: 'Blog',
+  DESCRIPTION: 'Writing on topics I am passionate about.',
+}
+
+// Projects Page
+export const PROJECTS: Page = {
+  TITLE: 'Projects',
+  DESCRIPTION: 'Recent projects I have worked on.',
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: 'Search',
+  DESCRIPTION: 'Search all posts and projects by keyword.',
+}
+
+// Study Page
+export const STUDIES = [
+  {
+    title: 'Information security engineering',
+    institution: 'Universidad de Manizales',
+    link: 'https://umanizales.edu.co/',
+    date: '2024-2028',
+  },
+  {
+    title: '...',
+    institution: 'Platzi',
+    link: 'https://platzi.com/',
+    date: '2018 - 2022',
+  },
+  {
+    title: '...',
+    institution: 'Udemy',
+    link: 'https://www.udemy.com/',
+    date: '2018 - 2022',
+  },
+  {
+    title: '...',
+    institution: 'Youtube',
+    link: 'https://www.youtube.com/@midulive',
+    date: '2018 - 2022',
+  },
+  
+]
+
+export const EXPERIENCE = [
+  
+]
